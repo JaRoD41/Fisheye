033d658f4c1228be8990b144f443088565224f05
JaRoD41 <laurent.trinco@gmail.com>
2023-01-17 16:01:39 +0100

    fetch OK

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,10 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
+    const cityName = `${city}, ${country}`
+    const tag = tagline
+    const rate = `${price}€/jour`
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
@@ -9,8 +12,17 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const home = document.createElement( 'city' );
+        home.textContent = cityName
+        const sentence = document.createElement( 'sentence' );
+        sentence.textContent = tag
+        const hourRate = document.createElement( 'hourRate' );
+        hourRate.textContent = rate
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(home)
+        article.appendChild(sentence)
+        article.appendChild(hourRate)
         return (article);
     }
     return { name, picture, getUserCardDOM }
