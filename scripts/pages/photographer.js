4d9463ad9ed5c133c47e1673c986778e901c9420
JaRoD41 <laurent.trinco@gmail.com>
2023-01-19 15:24:52 +0100

    fetch photographe by ID ok

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,3 +1,21 @@
 let url = new URL(location.href) // declare an variable to pick the actual URL
-let photographerPageId = url.searchParams.get("id") // get the id of the photographer from the URL
+let photographerPageId = url.searchParams.get('id') // get the id of the photographer from the URL
 
+async function getPhotographerInfos() {
+	let photographerInfos = []
+	let arrayOfPhotographers = []
+	await fetch('../../data/photographers.json')
+		.then((res) => res.json())
+		.then((data) => {
+			arrayOfPhotographers = data.photographers // filter apply to select the only object that matches the id found in the URL
+			photographerInfos = arrayOfPhotographers.filter((person) => {
+				return person.id == photographerPageId
+			})
+		})
+
+	return {
+		photographerInfos: [...photographerInfos],
+	}
+}
+
+getPhotographerInfos()
