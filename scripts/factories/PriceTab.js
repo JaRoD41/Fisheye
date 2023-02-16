ee3d9bd612f22d0dc10592862ec2a253e4ad8f14
JaRoD41 <laurent.trinco@gmail.com>
2023-02-16 09:35:57 +0100

    séparation des factories

---
+++ scripts/factories/PriceTab.js
@@ -0,0 +1,24 @@
+class PriceTabFactory {
+	constructor(photographerPrice, totalLikes) {
+		this.photographerPrice = photographerPrice
+		this.totalLikes = totalLikes
+	}
+	createPriceRateTab() {
+		const photographerTab = document.querySelector('.ratePriceLabel')
+		const container = document.createElement('div')
+		const likesSpan = document.createElement('span')
+		const likesImg = document.createElement('img')
+		const priceSpan = document.createElement('span')
+
+		likesSpan.textContent = this.totalLikes
+		likesSpan.classList.add('totalLikes')
+		likesImg.setAttribute('aria-label', 'nombre total de likes du photographe')
+		likesImg.setAttribute('src', '../../assets/icons/black-heart.svg')
+		priceSpan.textContent = `${this.photographerPrice}€/jour`
+
+		container.appendChild(likesSpan)
+		container.appendChild(likesImg)
+		photographerTab.appendChild(container)
+		photographerTab.appendChild(priceSpan)
+	}
+}
