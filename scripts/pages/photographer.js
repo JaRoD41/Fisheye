29869e97a313a35cd596b86990de8e6f518f1304
JaRoD41 <laurent.trinco@gmail.com>
2023-03-02 20:11:24 +0100

    sort selector changement tactique

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,6 +4,13 @@ let medias = []
 let sortedMedias = []
 let eachMedia = []
 
+const sortSelectors = document.querySelectorAll('.sort-selector')
+const buttonTitle = document.getElementById('sort-name')
+
+//tri par défaut -> popularité
+const defaultOption = 'Popularité'
+buttonTitle.textContent = defaultOption
+
 //récupération des données du photographe pour affichage
 async function displayData(photographerInfos, medias) {
 	let totalLikes = []
@@ -11,32 +18,34 @@ async function displayData(photographerInfos, medias) {
 	const gallery = document.querySelector('.photograph-gallery')
 	const dropdown = document.querySelector('.dropdown-filter')
 	const selectElement = document.getElementById('filter-button')
-	const arrow = document.querySelector('#filter-button span')
+	const arrow = document.querySelector('.arrowSort')
 
 	const filterMenu = document.getElementById('filter-menu')
 	const selectorsList = document.querySelectorAll('.dropdown-filter li')
 	const option1 = document.getElementById('filter-option1')
 	let choiceIndex = 0
-	const sortSelectors = document.querySelectorAll('.sort-selector')
 
 	//event listener pour clic sur le bouton de tri
 
 	selectElement.addEventListener('click', () => {
+		console.log('clic sur le bouton de tri');
 		if (
-			dropdown.classList.contains('active') &&
-			dropdown.getAttribute('style') === 'height: 150px;' &&
+			selectElement.classList.contains('active') &&
+			filterMenu.classList.contains('active') &&
+			filterMenu.getAttribute('style') === 'height: 159px;' &&
 			arrow.classList.contains('active')
 		) {
-			dropdown.classList.remove('active')
+			selectElement.classList.remove('active')
+			filterMenu.classList.remove('active')
 			arrow.classList.remove('active')
-			dropdown.setAttribute('style', '')
-			selectElement.setAttribute('aria-expanded', 'false')
+			filterMenu.setAttribute('style', '')
 		} else {
-			selectElement.setAttribute('aria-expanded', 'true')
-			dropdown.classList.toggle('active')
+			selectElement.classList.toggle('active')
+			filterMenu.classList.toggle('active')
 			arrow.classList.toggle('active')
-			dropdown.setAttribute('style', 'height: 150px;')
+			filterMenu.setAttribute('style', 'height: 159px;')
 		}
+
 		//test conditions supplementaires pour le focus
 
 		if (selectElement.getAttribute('aria-expanded') === 'false') {
@@ -48,8 +57,15 @@ async function displayData(photographerInfos, medias) {
 			filterMenu.setAttribute('aria-hidden', 'true')
 		}
 
+		// -------------- TEST ---------------
+		// sortSelectors.forEach((selector) => {
+		// 	selector.addEventListener('click', () => {
+		// 		console.log('selector :', selector);
+		// 	})
+		// })
+		// ----------------- FIN TEST -----------------
 		//event listener pour clic sur un des choix de tri
-
+		//--------------------------------------------
 		filterMenu.addEventListener('click', (event) => {
 			event.preventDefault()
 			event.stopPropagation()
@@ -65,24 +81,19 @@ async function displayData(photographerInfos, medias) {
 			choiceIndex = selectorsArray.indexOf(selectedListItem)
 			//modification de l'option affichée dans le bouton de tri
 
-			// TEST D'UNE FONCTION DE GESTION DE L'OPTION DE TRI CHOISIE
-			
-			// FIN DU TEST
-
 			// option1.classList.remove('sort-1')
 			// option1.classList.add(`sort-${rank}`)
 			// selectedListItem.classList.remove(`sort-${rank}`)
 			// selectedListItem.classList.add('sort-1')
 			//ajout de la classe selected sur l'option choisie
 
-			const choice = selectorsArray.find(
-				(selector) => selector.getAttribute('data-filter-value') === option
-			)
-			const isAriaSelected = selectorsArray.find(
-				(selector) => selector.getAttribute('aria-selected') === 'true'
-			)
+			// const choice = selectorsArray.find(
+			// 	(selector) => selector.getAttribute('data-filter-value') === option
+			// )
+			// const isAriaSelected = selectorsArray.find(
+			// 	(selector) => selector.getAttribute('aria-selected') === 'true'
+			// )
 			if (
-				// isAriaSelected.getAttribute('aria-selected') === 'true' &&
 				selectedListItem.classList.contains('selected') &&
 				dropdown.classList.contains('active')
 			) {
@@ -93,13 +104,8 @@ async function displayData(photographerInfos, medias) {
 					selector.classList.remove('selected')
 				)
 				selectedListItem.classList.add('selected')
-				// option1.setAttribute('aria-selected', false)
-				// selectedListItem.setAttribute('aria-selected', true)
 				dropdown.classList.remove('active')
 			}
-			console.log('option choisie :', option)
-			console.log('choice Index :', choiceIndex)
-			console.log('rank :', rank)
 
 			//tri des médias
 			sortedMedias = sort(medias, option)
@@ -118,11 +124,10 @@ async function displayData(photographerInfos, medias) {
 			})
 			addLikeListeners()
 		})
+		//fin du deuxième event listener
 	})
-	//fin de l'event listener
+	//fin du premier event listener
 
-	//tri par défaut -> popularité
-	const defaultOption = 'popularite'
 	sortedMedias = sort(medias, defaultOption)
 	sortedMedias.forEach((eachMedia, currentMediaIndex) => {
 		eachMedia = eachMedia
@@ -145,17 +150,6 @@ async function displayData(photographerInfos, medias) {
 	photographerSection.getPhotographerHeader()
 	addLikeListeners()
 }
-
-// function eventHandler(event) {
-// 	if (event.type === 'click') {
-// 		sortToggle()
-// 		sortChoice(event)
-// 	} else if (event.type === 'keydown' && event.code === 'Space') {
-// 		console.log('espace')
-// 	} else if (event.type === 'keydown' && event.code === 'Enter') {
-// 		console.log('entrée')
-// 	}
-// }
 
 async function init() {
 	// Récupère les datas du photographe
